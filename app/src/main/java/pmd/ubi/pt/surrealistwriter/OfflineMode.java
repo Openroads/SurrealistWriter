c6df79e8a20d47f62cab7d361eb126201211b9f1
TiagoMunder <tiago_7230@hotmail.com>
2016-11-26 13:55:10 +0000

    test

--- app/src/main/java/pmd/ubi/pt/surrealistwriter/OfflineMode.java
+++ app/src/main/java/pmd/ubi/pt/surrealistwriter/OfflineMode.java
@@ -1,7 +1,9 @@
 package pmd.ubi.pt.surrealistwriter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class OfflineMode extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class OfflineMode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline_mode);
+    }
+
+    public void gameplay(View v){
+        Intent gameplayIntent = new Intent(this, Gameplay.class);
+        startActivity(gameplayIntent);
+
+
     }
 }
