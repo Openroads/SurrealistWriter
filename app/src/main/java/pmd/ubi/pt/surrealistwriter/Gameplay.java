39a991a9490ce855ee24b259ca9e232cb2e9f1d5
TiagoMunder <tiago_7230@hotmail.com>
2016-11-26 13:46:35 +0000

    test

---
+++ app/src/main/java/pmd/ubi/pt/surrealistwriter/Gameplay.java
@@ -0,0 +1,18 @@
+package pmd.ubi.pt.surrealistwriter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Win 8 on 26/11/2016.
+ */
+
+public class Gameplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+
+    }
+}
