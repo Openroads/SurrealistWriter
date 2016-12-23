0c79ee91e4a93fd271523d308cdaf8403b80987c
Diogo Monteiro <jdsasm@hotmail.com>
2016-12-23 17:40:08 +0000

    Added Players Name Activity created; Added Color Picker; Added Verification for Player Names; Added Verification for Colors.

--- app/src/main/java/pmd/ubi/pt/surrealistwriter/PlayerName.java
+++ app/src/main/java/pmd/ubi/pt/surrealistwriter/PlayerName.java
@@ -1,16 +1,34 @@
 package pmd.ubi.pt.surrealistwriter;
 
 import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import petrov.kristiyan.colorpicker.ColorPicker;
+
 
 public class PlayerName extends AppCompatActivity {
 
     int iNumPlayers;
     int iNumCharacters;
     int iGameMode;
+    int currentPos;
+    private EditText[] mEditTextPlayers;
+    private Button[] mColorButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +39,109 @@ public class PlayerName extends AppCompatActivity {
         iNumCharacters = getIntent().getExtras().getInt("numCharacters");
         iGameMode      = getIntent().getExtras().getInt("gameMode");
 
-        RelativeLayout rLayout = (RelativeLayout) findViewById(R.id.activity_player_name);
-        RelativeLayout.LayoutParams rLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        EditText etPlayerNameSelection = new EditText(getApplicationContext());
-        etPlayerNameSelection.setLayoutParams(rLayoutParams);
-        rLayout.addView(etPlayerNameSelection);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.editTextGroupLayout);
+        generateEditTexts(linearLayout, iNumPlayers);
+
+    }
+
+    public void generateEditTexts(LinearLayout linearLayout, final int numPlayers){
+
+        mEditTextPlayers          = new EditText[numPlayers];
+        LinearLayout lineLayout[] = new LinearLayout[numPlayers];
+        mColorButtons             = new Button[numPlayers];
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+        for(int i=0;i<numPlayers;i++){
+            currentPos = i;
+            lineLayout[i]       = new LinearLayout(this);
+            lineLayout[i].setOrientation(LinearLayout.HORIZONTAL);
+            mEditTextPlayers[i] = new EditText(this);
+            mEditTextPlayers[i].setLayoutParams(params);
+            mEditTextPlayers[i].setGravity(Gravity.CENTER);
+            mEditTextPlayers[i].setHint("Player "+(i+1));
+            mEditTextPlayers[i].setMaxLines(1);
+            mEditTextPlayers[i].setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+            lineLayout[i].addView(mEditTextPlayers[i]);
+            mColorButtons[i] = new Button(this);
+            mColorButtons[i].setLayoutParams(params);
+            mColorButtons[i].setId(i);
+            mColorButtons[i].setHint("Set Color");
+            mColorButtons[i].setBackgroundColor(0);
+            mColorButtons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final View tempView = view;
+                    ColorPicker colorPicker = new ColorPicker(PlayerName.this);
+                    colorPicker.show();
+                    Log.d("DEBUG","Prev color: "+mColorButtons[tempView.getId()].getBackground());
+                    colorPicker.setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
+                        @Override
+                        public void onChooseColor(int position,int color) {
+                            boolean flag = true;
+                            int btColor;
+                            Drawable btBackground;
+                            for(int i=0;i<numPlayers;i++){
+                                btBackground = mColorButtons[i].getBackground();
+                                btColor = ((ColorDrawable) btBackground).getColor();
+                                if(color == btColor) {
+                                    flag = false;
+                                }
+                            }
+                            if(flag == false){
+                                Toast.makeText(PlayerName.this, "Color already selected",
+                                        Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                            mColorButtons[tempView.getId()].setBackgroundColor(color);
+                            mColorButtons[tempView.getId()].setHint("Color Set");
+                        }
+
+                        @Override
+                        public void onCancel(){
+                            // put code
+                        }
+                    });
+                }
+            });
+            lineLayout[i].addView(mColorButtons[i]);
+            linearLayout.addView(lineLayout[i]);
+        }
+
+    }
+
+    public void onClickPlayGame(View view){
+        boolean flag = true;
+        for(int i=0;i<iNumPlayers;i++)
+            for(int j=0;j<iNumPlayers;j++){
+                if(i==j)
+                    continue;
+                if(mEditTextPlayers[i].getText().toString().equals("")){
+                    Toast.makeText(this, "Every player must have a name!", Toast.LENGTH_SHORT).show();
+                    continue;
+                }
+                if(mEditTextPlayers[i].getText().toString()
+                        .equals(mEditTextPlayers[j].getText().toString())) {
+                    Toast.makeText(this, "Name already in use!", Toast.LENGTH_SHORT).show();
+                    Log.d("Debug",""+ i+" "+j);
+                    flag = false;
+                }
+            }
+
+        for(int i=0;i<iNumPlayers;i++) {
+            Drawable btBackground = mColorButtons[i].getBackground();
+            int btColor = ((ColorDrawable) btBackground).getColor();
+
+            if (btColor == 0) {
+                Toast.makeText(this, "You must select a color!", Toast.LENGTH_SHORT).show();
+                flag = false;
+            }
+        }
+
+
+            if(flag == false)
+                Log.d("Debug","Won't jump to next activity!");
+            else
+                Log.d("Debug","Will jump to next activity!");
 
     }
 }
