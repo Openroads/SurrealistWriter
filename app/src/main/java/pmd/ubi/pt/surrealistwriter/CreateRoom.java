f23d606270ebb562dee1bdeeeb9c653689ebf231
daren <daren22@o2.pl>
2016-12-27 21:00:35 +0000

    checking data to create room

--- app/src/main/java/pmd/ubi/pt/surrealistwriter/CreateRoom.java
+++ app/src/main/java/pmd/ubi/pt/surrealistwriter/CreateRoom.java
@@ -12,6 +12,7 @@ public class CreateRoom extends AppCompatActivity {
 
     EditText roomNameET;
     EditText maxNumPlayersET;
+    EditText numRoundsET;
     EditText numCharactersET;
     ToggleButton roomModeTogButt;
 
@@ -26,15 +27,17 @@ public class CreateRoom extends AppCompatActivity {
     public void create_tableOnClick(View view) {
         roomNameET =        (EditText) findViewById(R.id.room_name_ET);
         maxNumPlayersET =   (EditText) findViewById(R.id.num_players_ET);
+        numRoundsET =       (EditText) findViewById(R.id.num_rounds_ET);
         numCharactersET =   (EditText) findViewById(R.id.num_chars_ET);
         roomModeTogButt = (ToggleButton) findViewById(R.id.roomModeTG);
 
         String roomName = roomNameET.getText().toString();
         String maxNumPlayers = maxNumPlayersET.getText().toString();
+        String numRounds = numRoundsET.getText().toString();
         String numCharacters = numCharactersET.getText().toString();
         String gameMode = roomModeTogButt.getText().toString();
         //valid data to use
-        if(checkData(roomName,maxNumPlayers,numCharacters))
+        if(checkData(roomName,maxNumPlayers,numCharacters,numRounds))
         {
 
             Toast.makeText(this, "przechodzi"+gameMode, Toast.LENGTH_SHORT).show();
@@ -43,22 +46,25 @@ public class CreateRoom extends AppCompatActivity {
 
 
     }
-    public boolean checkData(String name,String NumPlayers, String NumCharacters) {
+    public boolean checkData(String name,String NumPlayers, String NumCharacters,String NumRounds) {
         int iNumPlayers = 0;
         int iNumCharacters = 0;
+        int iNumRounds = 0;
         if (name.isEmpty()) {
             Toast.makeText(this, "You must enter name of room!", Toast.LENGTH_SHORT).show();
             return false;
-        }else if(NumPlayers.isEmpty())
-        {
+        } else if (NumPlayers.isEmpty()) {
             Toast.makeText(this, "You must enter number of players!", Toast.LENGTH_SHORT).show();
             return false;
-        }
-        else if(NumCharacters.isEmpty()){
+        } else if (NumCharacters.isEmpty()) {
             Toast.makeText(this, "You must enter number of characters!", Toast.LENGTH_SHORT).show();
             return false;
-        }else{
+        } else if (NumRounds.isEmpty()) {
+            Toast.makeText(this, "You must enter number of rounds!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
             iNumPlayers = Integer.parseInt(NumPlayers);
+            iNumRounds = Integer.parseInt(NumRounds);
             iNumCharacters = Integer.parseInt(NumCharacters);
         }
         // Checking Number of Players
@@ -84,6 +90,19 @@ public class CreateRoom extends AppCompatActivity {
             Toast.makeText(this, "Minimum of " + iNumPlayers * 2 + " characters!", Toast.LENGTH_SHORT).show();
             return false;
         }
+
+        // Checking Number of Rounds
+
+        if (iNumRounds > 0) {
+            if (iNumRounds >= 20) {
+                Toast.makeText(this, "Maximum of 20 rounds!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else {
+            Toast.makeText(this, "Minimum of 1 round!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
         return true;
     }
 }
