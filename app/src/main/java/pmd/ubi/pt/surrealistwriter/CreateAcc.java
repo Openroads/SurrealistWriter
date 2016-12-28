91fe5a1d25225985006fdd8032d54ae366de50ed
daren <daren22@o2.pl>
2016-12-28 20:16:39 +0000

    Start to create account online with webservice

--- app/src/main/java/pmd/ubi/pt/surrealistwriter/CreateAcc.java
+++ app/src/main/java/pmd/ubi/pt/surrealistwriter/CreateAcc.java
@@ -1,25 +1,34 @@
 package pmd.ubi.pt.surrealistwriter;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.Date;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 import pmd.ubi.pt.objects.User;
 
 public class CreateAcc extends AppCompatActivity
 {
-
+    // Progress Dialog Object
+    private ProgressDialog prgDialog;
     //UI references
     private EditText mUserNameView;
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
-    private Button mRegisterButton;
+    private EditText mErrorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,8 +40,7 @@ public class CreateAcc extends AppCompatActivity
         mUserNameView = (EditText) findViewById(R.id.reg_username);
         mEmailView = (AutoCompleteTextView) findViewById(R.id.reg_email);
         mPasswordView = (EditText) findViewById(R.id.reg_password);
-        mRegisterButton = (Button) findViewById(R.id.registration_button);
-
+        mErrorView = (EditText) findViewById(R.id.error_regist_TW);
 
     }
 
@@ -44,10 +52,85 @@ public class CreateAcc extends AppCompatActivity
 
         Date data = new Date();
 
-        User newUser = new User(1, username, email, password, data);
+        /*User newUser = new User(1, username, email, password, data);
 
-        Toast.makeText(this, newUser.toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, newUser.toString(), Toast.LENGTH_LONG).show();*/
+        RequestParams params = new RequestParams();
+
+        params.put("name", username);
+        // Put Http parameter username with value of Email Edit View control
+        params.put("username", email);
+        // Put Http parameter password with value of Password Edit View control
+        params.put("password", password);
+        // Invoke RESTful Web Service with Http parameters
+        invokeWS(params);
 
 
+    }
+    public void invokeWS(RequestParams params){
+        // Show Progress Dialog
+        prgDialog.show();
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://127.0.1.1:8080/SurrealistWriterRESTful/register/doregister",params ,new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(responseBody.toString());//?????????
+                    // When the JSON response has status boolean value assigned with true
+                    if(obj.getBoolean("status")){
+                        Toast.makeText(getApplicationContext(), "You are successfully logged in!", Toast.LENGTH_LONG).show();
+                        // Navigate to Home screen
+                        navigatetoHomeActivity();
+                    }
+                    // Else display error message
+                    else{
+                        mErrorView.setText(obj.getString("error_msg"));
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    public void navigatetoHomeActivity(){
+        Intent homeIntent = new Intent(getApplicationContext(),MainActivity.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+    }
+    public void navigatetoRegisterActivity(View view){
+        Intent loginIntent = new Intent(getApplicationContext(),CreateAcc.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(loginIntent);
+    }
+    private void setDefaultValues(){
+        mUserNameView.setText("");
+        mEmailView.setText("");
+        mPasswordView.setText("");
     }
 }
